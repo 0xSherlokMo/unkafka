8ebfb769fdc72dfb6700659de4eb7739e2e07d54
Mohammad Salah <salah@abwab.ai>
2025-01-03 22:03:46 +0200

    Implement KafkaResponse encoding and send response on connection acceptance

--- app/server.go
+++ app/server.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
+
+type Headers struct {
+	CorrelatioId uint32
+}
+
+type KafkaResponse struct {
+	Size    uint32
+	Headers Headers
+}
+
+func (k *KafkaResponse) Encode() []byte {
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, k.Size)
+	if err != nil {
+		fmt.Println("Error encoding size: ", err.Error())
+		os.Exit(1)
+	}
+	err = binary.Write(&buffer, binary.BigEndian, k.Headers.CorrelatioId)
+	if err != nil {
+		fmt.Println("Error encoding correlation id: ", err.Error())
+		os.Exit(1)
+	}
+
+	return buffer.Bytes()
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,9 +42,21 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	response := KafkaResponse{
+		Size: 10,
+		Headers: Headers{
+			CorrelatioId: 7,
+		},
+	}
+
+	_, err = conn.Write(response.Encode())
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
 		os.Exit(1)
 	}
 
